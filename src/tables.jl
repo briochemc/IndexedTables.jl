61bf323bc6473d18fee907c1859f3b5c0e995db3
Jacob Quinn <quinn.jacobd@gmail.com>
2019-03-11 12:01:19 -0600

    Just hard-code the Tables.columnaccess and Tables.rowaccess traits for Tables.jl interface (#221)

--- src/tables.jl
+++ src/tables.jl
@@ -3,10 +3,10 @@ Tables.istable(::Type{<:IndexedTable}) = true
 
 Tables.materializer(t::IndexedTable) = table
 
-Tables.columnaccess(::Type{<:IndexedTable}) = Tables.columnaccess(StructArray)
+Tables.columnaccess(::Type{<:IndexedTable}) = true
 Tables.columns(t::IndexedTable) = Tables.columns(columns(t))
 
-Tables.rowaccess(::Type{<:IndexedTable}) = Tables.rowaccess(StructArray)
+Tables.rowaccess(::Type{<:IndexedTable}) = true
 Tables.rows(t::IndexedTable) = Tables.rows(rows(t))
 
 # table(x; copy=false, kw...) = table(Tables.columntable(x); copy=copy, kw...)
