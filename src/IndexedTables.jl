42e32066271efa0320adf830d2cc0a5d7399b5b9
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-04-23 14:06:59 +0100

    use IndexedTables rather than StructArrays utils

--- src/IndexedTables.jl
+++ src/IndexedTables.jl
@@ -4,8 +4,8 @@ using PooledArrays, SparseArrays, Statistics, WeakRefStrings
 
 using OnlineStatsBase: OnlineStat, fit!
 
-using StructArrays: StructVector, StructArray, foreachfield, fieldarrays,
-    collect_structarray, staticschema, ArrayInitializer, refine_perm!, collect_structarray,
+using StructArrays: StructVector, StructArray, fieldarrays,
+    staticschema, ArrayInitializer, refine_perm!, collect_structarray,
     collect_empty_structarray, grow_to_structarray!, collect_to_structarray!, replace_storage,
     GroupPerm, GroupJoinPerm, roweq, rowcmp
 
